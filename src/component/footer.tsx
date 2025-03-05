30813a64295c62e54e696debae8cd523cf746872
Flames <isaacomenuche@gmail.com>
2025-03-05 15:20:58 +0100

    style: fix footer alignment issue

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -2,24 +2,28 @@ import React from "react"
 
 function Footer() {
   return (
-    <>
-      <div className="flex flex-col items-center text-center bg-[#1B1B1B] rounded-3xl text-[#FFF] lg:mx-[80px] mt-[120px] lg:mt-[240px] mb-[190px] pb-8 h-fit lg:h-[592px] max-w-[300px] md:max-w-[768px] lg:max-w-[1280px]">
-        <div className=" items-center w-[300px] lg:w-[627px] pt-[89px]">
-          <p className="font-bold text-5xl/[68px]">
-            Wanting to take your business to the next level?
-          </p>
-          <p className="text-[#ECECEC] lg:font-[450px] text-2xl/[32px]">
-            Are you willing to take your business to the next level, register
-            with us today.
-          </p>
-          <button className="border border-[#0063CC] rounded-xl bg-[#FFF] text-[#2A2A2A] py-[12px] pr-[16px] pl-[24px] mt-[12px]">
+    <div className="space-y-[134px] px-[5.6%]">
+      <div className="mx-auto max-w-[1280px] rounded-3xl bg-[#1B1B1B] text-center text-[#FFF]">
+        <div className="flex flex-col items-center space-y-8 px-4 py-8 lg:pt-[89px] lg:pb-[219px]">
+          <div className="space-y-3 lg:w-[627px]">
+            <p className="text-3xl font-bold lg:text-5xl lg:leading-[68px]">
+              Wanting to take your business to the next level?
+            </p>
+            <p className="text-2xl leading-[32px] text-[#ECECEC] lg:text-2xl lg:font-[450px]">
+              Are you willing to take your business to the next level, register
+              with us today.
+            </p>
+          </div>
+          <button className="mt-[12px] rounded-xl border border-[#0063CC] bg-[#FFF] py-[12px] pr-[16px] pl-[24px] text-[#2A2A2A]">
             Get Started
           </button>
         </div>
       </div>
-      <p className="text-center">© 2025, Powered by HNG12</p>
-    </>
-  );
+      <p className="py-4 text-center text-base text-[#84919D]">
+        © 2025, Powered by HNG12
+      </p>
+    </div>
+  )
 }
 
 export default Footer
