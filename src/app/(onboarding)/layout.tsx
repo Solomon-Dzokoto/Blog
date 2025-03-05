58e805e456be8d15630242fe2d279c2d79fe50ec
Flames <isaacomenuche@gmail.com>
2025-03-05 21:16:17 +0100

    style: fix background image repeat

--- src/app/(onboarding)/layout.tsx
+++ src/app/(onboarding)/layout.tsx
@@ -5,11 +5,12 @@ export default function OnboardingLayout({
 }>) {
   return (
     <body
-      className={`min-h-screen bg-cover bg-center`}
+      className={`min-h-screen min-w-screen bg-cover bg-center`}
       style={{
         backgroundImage: "url('/images/onboarding-bg.png')",
         backgroundColor: "#1E1E1EBF",
         backgroundBlendMode: "overlay",
+        backgroundRepeat: "no-repeat",
       }}
     >
       {children}
