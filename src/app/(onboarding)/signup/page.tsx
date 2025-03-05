0f24233dedaf28b08b21b482cff86aaf355eed37
Flames <isaacomenuche@gmail.com>
2025-03-06 00:30:29 +0100

    fix: fix vertical scroll

--- src/app/(onboarding)/signup/page.tsx
+++ src/app/(onboarding)/signup/page.tsx
@@ -3,7 +3,7 @@ import SignUpFlow from "./sign-up-flow"
 
 export default function SignUp() {
   return (
-    <div className="mt-[18.5%] flex flex-col items-center space-y-20 px-[4.5%]">
+    <div className="flex h-screen flex-col items-center justify-center space-y-20 px-[4.5%]">
       <div
         className="h-[72px] w-[284px]"
         style={{
