db0f78133d1a7f472a6ab48ecefd1aea26cd8d76
Miracle Ibharokhonre <miracleosemudiahen@hotmail.com>
2025-03-05 16:56:22 +0100

    chore: adjusted landing page flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <main>
       <HeroSection />
-      <WhySection />
       <HowItWorksSection />
+      <WhySection />
       <BusinessOwnerReviewsSection />
       <Footer />
     </main>
