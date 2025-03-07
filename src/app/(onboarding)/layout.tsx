698e04cfd370077263cfc6802c95dc6dea8d93f0
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-03-07 21:00:41 +0000

    feat: refactor onboarding layout and remove unused sign-up flow component

--- src/app/(onboarding)/layout.tsx
+++ src/app/(onboarding)/layout.tsx
@@ -2,7 +2,7 @@ export default function OnboardingLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="min-h-screen w-full bg-[#1E1E1EBF] bg-[url('/images/onboarding-bg.png')] bg-cover bg-center bg-fixed bg-no-repeat [background-blend-mode:overlay]">
+    <div className="min-h-screen  w-full bg-[#1E1E1EBF] bg-[url('/images/onboarding-bg.png')] bg-cover bg-center bg-fixed bg-no-repeat [background-blend-mode:overlay]">
       <div className="flex h-screen flex-col items-center justify-center space-y-20 px-[4.5%]">
         <div className="h-[72px] w-[284px] bg-[url('/images/Logo.png')] bg-cover bg-no-repeat" />
         {children}
